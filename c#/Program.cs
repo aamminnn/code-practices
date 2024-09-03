0d5883972dcc46cf3c2e5ccd0fac5dc9eade60f2
Amin Affandi <aminaffandi@github.com>
2024-09-03 15:00:41 +0800

    c# matrix multiplication

--- c#/Program.cs
+++ c#/Program.cs
@@ -4,19 +4,19 @@ public class Program {
     public static void Main(String[] args) {
         System.Console.WriteLine("Hello, World!");
 
-        // 1
-        // int arraySize = 10; // Size of the array
-        // int minValue = 1;   // Minimum value for random numbers
-        // int maxValue = 100; // Maximum value for random numbers
-        // int[] randomArray = Utils.randomNumber(arraySize, minValue, maxValue);
-        // List<int> myarray = new List<int> (randomArray);
-        // Console.WriteLine("Unsorted Array: " + string.Join(", ", myarray));
-        // List<int> sortedArray = Utils.sortDescending(myarray);
-        // Console.WriteLine("Unsorted Array: " + string.Join(", ", sortedArray));
-        // int secondElement = Utils.findSecondElement(sortedArray);
-        // System.Console.WriteLine("second element: "+ secondElement);
+        // 1. 2nd largest element in array
+        int arraySize = 10; // Size of the array
+        int minValue = 1;   // Minimum value for random numbers
+        int maxValue = 100; // Maximum value for random numbers
+        int[] randomArray = Utils.randomNumber(arraySize, minValue, maxValue);
+        List<int> myarray = new List<int> (randomArray);
+        Console.WriteLine("Unsorted Array: " + string.Join(", ", myarray));
+        List<int> sortedArray = Utils.sortDescending(myarray);
+        Console.WriteLine("Unsorted Array: " + string.Join(", ", sortedArray));
+        int secondElement = Utils.findSecondElement(sortedArray);
+        System.Console.WriteLine("second element: "+ secondElement);
 
-        //2
+        //2 Array multiplication
         int [,] matrixA = {
             {1,2,3},
             {4,5,6}
